209404ab53d626bb8eb78c3b43b92d534f71ef5a
lesasi <nevinusa@gmail.com>
2021-06-01 14:16:43 +0530

    Made basic component for handling the input of links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,81 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import convert from '../actions/convert';
-import download from '../actions/download';
+import InputComponent from './InputComponent';
 
 const App = () => {
+    
+    const [inputList, setInputList] = useState([0]);
 
-    const [link, setLink] = useState('');
-    const [token, setToken] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const renderInputList = () => {
+        return inputList.map((input, index) => {
+            return(
+                <div>
+                    <InputComponent key={index}/>
+                </div>
+            );
+        })
+    }
 
-    const onConvertClick = async (e) => {
+    const addNewInput = (e) => {
         e.preventDefault();
-
-        setLoading(true);
-        const { data, error } = await convert(link);
-        setLoading(false);
-        
-        if(error) {
-            console.log(error);
-            return;
-        }
-        setToken(data.token)
-        console.log(data.meta)
-    };
-
-    const onDownloadClick = async (e) => {
-        e.preventDefault();
-        const { response, error } = await download(token);
-        if(error) {
-            console.log(error);
-            return;
-        }
-        const filename =  response.headers['content-disposition'].split('filename=')[1];
-
-        const url = window.URL.createObjectURL(new Blob([response.data]), { type: 'audio/mp3' });
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', filename.slice(1,-1)); //or any other extension
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
+        setInputList([...inputList, 0]);
+        console.log(inputList)
     }
 
     return(
-        <div>
-            <input 
-                type="text" 
-                id="link"
-                placeholder="Youtube link" 
-                value={link}
-                onChange={ (e) => setLink(e.target.value) }
-                required
-            />
-            <button
-                onClick={onConvertClick}
-                id="convert-button"
+        <div className="main">
+            <h1>Add Links</h1>
+            {renderInputList()}
+            <div 
+                className="add-new-input"
+                onClick={addNewInput}
             >
-                Convert
-            </button>
-            {
-                token ? (
-                    <button
-                        onClick={onDownloadClick}
-                        id="download-button"
-                    >
-                        Download
-                    </button>
-                ) : null
-            }
-            {
-                loading ? (
-                    <div>
-                        Loading...
-                    </div>
-                ) : null
-            }
+                Add New Link
+            </div>
         </div>
     );
 };
